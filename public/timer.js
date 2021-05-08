cbeffb34d54691acc9d5501a50b195b282e50b64
Nadav <nadav.moran@gmail.com>
2021-05-08 22:56:16 +0300

    Fixed timer sync

--- public/timer.js
+++ public/timer.js
@@ -2,7 +2,7 @@ const timer = document.getElementById('timer');
 
 var min = 0;
 var sec = 0;
-var mili = 0;
+var milli = 0;
 var stoptime = true;
 
 function startTimer() {
@@ -20,20 +20,20 @@ function stopTimer() {
 
 function timerCycle() {
     if (stoptime == false) {
-    mili = parseInt(mili);
+    milli = parseInt(milli);
     sec = parseInt(sec);
     min = parseInt(min);
 
-    mili += 1;
+    milli += 1;
 
-    if (mili == 100) {
+    if (milli == 10) {
       sec += 1;
-      mili = 0;
+      milli = 0;
     }
     if (sec == 60) {
       min += 1;
       sec = 0;
-      mili = 0;
+      milli = 0;
     }
     if (min == 60) {
       hr = hr + 1;
@@ -47,18 +47,15 @@ function timerCycle() {
     if (min < 10 || min == 0) {
       min = '0' + min;
     }
-    if (mili < 10 || mili == 0) {
-      mili = '0' + mili;
-    }
 
-    timer.innerHTML = min + ':' + sec + '.' + mili;
+    timer.innerHTML = min + ':' + sec + '.' + milli;
 
-    setTimeout("timerCycle()", 10);
+    setTimeout("timerCycle()", 100);
   }
 }
 
 function resetTimer() {
-  mili = 0;
+  milli = 0;
   sec = 0;
   min = 0;
   stopTimer();
