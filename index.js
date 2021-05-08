bb87fe3b7a49a71525e0f17cc56953d76446090b
Nadav <nadav.moran@gmail.com>
2021-05-08 11:02:29 +0300

    Fixed an error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.listen(3000), () => console.log('listen at 3000'))
+app.listen((3000), () => console.log('listen at 3000'))
 app.use(express.static('public'))
