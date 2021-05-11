8b65bb203de99283361c05e16bce1a14aa7fb9b2
Nadav <nadav.moran@gmail.com>
2021-05-11 12:26:33 +0300

    Changed tab name

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const app = express();
 app.listen((3500), () => console.log('listen at 3500'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
+
+const results = {};
+const names = {};
+
 app.post('/api', (request, response) => {
   const data = request.body;
-  console.log(data.time);
+  console.log(data);
+  if (data.name != null)
+    names[response] = data.name;
+  if (data.time != null)
+    results[names[response]] = time;
+  console.log(results);
 });
