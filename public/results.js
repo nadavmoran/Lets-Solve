64957b7071340997bb208a09d6258b33a0cb6bf9
Nadav <nadav.moran@gmail.com>
2021-05-18 00:33:52 +0300

    Fixed a mistake

--- public/results.js
+++ public/results.js
@@ -1,10 +1,9 @@
-//The server socket
+//The socket for sending the data
 const socket = io.connect("http://127.0.0.1:3500");
-
-// The HTML elements
-const results = document.getElementById("results");
 
 //Updates the results when getting a broadcast
 socket.on("broadcast", function(data) {
-  results.innerHTML = "hello" + "/n" + 'hello';
+  console.log(data);
+  results = document.getElementById("results");
+  results.innerHTML = data.name + ":" + data.results;
 });
