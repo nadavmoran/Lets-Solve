e6aad7c93cbbdc0c596af0e8806d682944b46875
Nadav <nadav.moran@gmail.com>
2021-05-17 19:27:14 +0300

    Remove unused modules

--- cube-solver/src/scramblers/corners.js
+++
@@ -1,9 +0,0 @@
-import getScrambleForPieces from './scramblePieces';
-
-const getScramble = () => getScrambleForPieces(
-  [],
-
-  [0, 1, 2, 3, 4, 5, 6, 7],
-);
-
-export default getScramble;
--- cube-solver/src/scramblers/tests/3x3.test.js
+++
@@ -1,8 +0,0 @@
-import getScramble from '../3x3';
-import { configureMathMock } from './utils';
-
-configureMathMock(98098097898);
-
-test('create a 3x3 scramble', () => {
-  expect(getScramble()).toEqual(expect.stringMatching(/^([FRUBLD][2']?\s*)+$/));
-});
