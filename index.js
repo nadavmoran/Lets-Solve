cc91d2e45b10c62d5c1453ef6c3027faba5076ce
Nadav <nadav.moran@gmail.com>
2021-05-18 11:22:59 +0300

    Print the ip

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+// Loading the needed modules
 const express = require("express");
 const app = express();
-const server = app.listen(3500, () => console.log("listen at 3500"));
+const os = require("os");
+
+// Getting the local ip
+var networkInterfaces = os.networkInterfaces();
+var ip = networkInterfaces["Ethernet"][3]["address"];
+
+// The port to listen
+const port = 3500;
+// Listening to port 3500
+const server = app.listen(port, () =>
+  console.log("listen at " + ip + " on port " + port)
+);
 const io = require("socket.io")(server);
 
 const results = {results: []};
 
+// Loading the code to pass to the client
 app.use(express.static("public"));
 app.use(express.json({limit: "1mb"}));
 
